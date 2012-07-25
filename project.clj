6c300502d3cd4688899e87fd7b78e84355ef26a6
Kevin Downey <redchin@gmail.com>
2012-07-24 18:59:33 -0700

    0.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject drawbridge-cljs "1.0.0-SNAPSHOT"
+(defproject drawbridge-cljs "0.0.1"
   :description "drawbridge client for clojurescript"
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :dev-dependencies [[lein-cljsbuild "0.2.4"]]
